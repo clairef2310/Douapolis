33db186d3dc5be1b7948e4c7b03fa8bcfe3d3c7d
Iona Sirach <iona.sirach@etu.univ-lyon1.fr>
2023-03-21 14:30:10 +0100

    modifs du context boolean de connexion

--- douapolis/src/testAuth/boolAuth.js
+++ douapolis/src/testAuth/boolAuth.js
@@ -1,6 +1,11 @@
-import React from "react";
-
-export default React.createContext({
-    isAuthenticated : false,
-    setIsAuthenticated : value => {}
-});
+import React, { useState, createContext } from "react";
+const AuthContext = createContext();
+const AuthProvider = (props) => {
+    const [authState, setAuthState] = useState({ isLogged: false });  
+    return (    
+        <AuthContext.Provider value={[authState, setAuthState]}>      
+            {props.children}
+        </AuthContext.Provider>
+    );
+};
+export { AuthContext, AuthProvider };
