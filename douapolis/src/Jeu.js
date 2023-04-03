a3611a1297ae43104c2cc9d890236890aa398858
Claire Fernandes <claire.fernandes@etu.univ-lyon1.fr>
2023-04-03 11:31:28 +0200

    ajout fonction déplacement pion

--- douapolis/src/Jeu.js
+++ douapolis/src/Jeu.js
@@ -13,11 +13,15 @@ function Jeu() {
     const [de2, setde2] = useState();
     const [total, setTotal] = useState();
     const [variant, setVariant] = useState('primary')
-    const [posx, setPosx] = useState(640);
-    const [posy, setPosy] = useState(630);
     const [count, setcount] = useState(0);
     const [player, setPlayer] = useState('Joueur 1');
-    const [posPion, setPosPion] = useState('Sud')
+    const [posx, setPosx] = useState(640);
+    const [posy, setPosy] = useState(630);
+    const [ncase, setCase] = useState("Départ");
+    const nomcase = ["Depart", "Nautibus", "Carte Bourse", "Astrée", "Taxe Cafèt", "Université Lyon 1", "Florel", "Carte Chance","Dubois", "Darwin","Simple Visite", "Berthollet","SSU","Thémis", "Cryo", "Gaston Berger","Jordan", "Carte Bourse", "Grignard", "Lippman","Parking Nautibus", "Géode","Carte Chance", "STAPS", "Atlas", "Insa Einstein", "Ariane", "Quai 43", "La SOIE", "Omega", "Allez en Prison", "Déambu", "Braconnier", "Carte Bourse", "Lwoff", "Insa Einstein", "Carte Chance", "Raulin", "Taxe Resto U", "Chevreul"];
+    const coordx = [640,570,510,445,390,330,265,200,145,80,10,10,10,10,10,10,10,10,10,10,80,140,200,265,330,385,450,510,570,635,635,635,635,635,635,635,635,635];
+    const coordy = [630, 630,630,630,630,630,630,630,630,630,645,570,500,450,380,320,260,200,140,80,10,10,10,10,10,10,10,10,10,10,10,80,140,200,260,320,380,450,510,570];;
+    const [indice, setIndice] = useState(0)
 
     //coordonnée case départ 
     //position x : 640
@@ -29,61 +33,15 @@ function Jeu() {
     };
 
     async function avancerPion(){
-        //fonction de choix aléatoir du dé
-        let de1 = Math.floor(Math.random() * (6 - 1) + 1);
-        let de2 = Math.floor(Math.random() * (6 - 1) + 1);
-        //total des dés
-        let total = de1 + de2;
+       //fonction de choix aléatoir du dé
+       let de1 = Math.floor(Math.random() * (6 - 1) + 1);
+       let de2 = Math.floor(Math.random() * (6 - 1) + 1);
+       //total des dés
+       let total = de1 + de2;
+
         //tableau de joueur pour faire les test avec changement de couleur pour les différencier
         let tab = ['primary','success','warning','danger'];
         let joueur = ['Joueur 1','Joueur 2','Joueur 3','Joueur 4']
-        //fonction de calcul pour avancer
-        let avancex = total * 62
-        let avancey = total * 62
-
-        //mise à jour des valeurs des constantes
-        setde1(de1);
-        setde2(de2);
-        setTotal(total); 
-        
-
-        if(posx === 15 && posy === 12){
-            setPosPion('Nord')
-        }else if(posx === 15 && posy === 12){
-            setPosPion('Est')
-        }
-       
-        if(posPion === "Sud"){
-            setPosx(posx-avancex);
-
-        }else if(posPion === "Ouest"){
-            setPosx(posx-avancey);
-            
-        }else if(posPion === "Nord"){
-            setPosx(posx+avancex);
-            
-        }else if(posPion === "Est"){
-            setPosx(posx+avancey);
-            
-        }else if(posx === 82 && posy === 630){
-            setPosx(15);
-            setPosy(650);
-            setPosPion('Ouest')
-            if(total > 1){
-                avancey = (total-1) * 62
-                setPosx(posx-avancey);
-            }	
-        }else if(posx === 635 && posy === 570){
-            setPosx(640);
-            setPosy(630);
-            setPosPion('Sud')
-            if(total > 1){
-                avancey = (total-1) * 62
-                setPosx(posx-avancex);
-            }	
-        }
-
-
 
         if(de1 !== de2){
             if(count === 3){
@@ -97,9 +55,21 @@ function Jeu() {
             }
         }
 
-        
+        //mise à jour des valeurs des constantes
+        setde1(de1);
+        setde2(de2);
+        setTotal(total); 
+
+        setIndice(indice+total);
+        if(indice >= nomcase.length -1){
+            setIndice(nomcase.length-indice);
+        }
+        setPosx(coordx[indice]);
+        setPosy(coordy[indice]);
+        setCase(nomcase[indice]);
     };
 
+    
     //formulaire et titre afficher sur la page 
     return(
         <div class="entete">
@@ -118,7 +88,9 @@ function Jeu() {
                                 Valeur du deuxième dé : {de2} <br/>
                                 Vous avancez de : {total}<br/>
                                 C'est au joueur {player} de jouer <br/>
-                                position pion : {posPion}
+                                Vous êtes sur la case : {ncase} <br/>
+                                coordonnée de x : {posx}<br/>
+                                coordonnéede y : {posy}<br/>
                             </p>
                         </div>     
                     <div style={{position:"absolute", left:`${posx}px`,top:`${posy}px`}}>
