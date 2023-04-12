363993ecc84b92f833c1bd1dd57c0b21c30072a1
Iona Sirach <iona.sirach@etu.univ-lyon1.fr>
2023-04-12 14:28:41 +0200

    tests sockets

--- douapolis/src/SalleAttente.js
+++ douapolis/src/SalleAttente.js
@@ -4,38 +4,73 @@ import Navigation from "./Navigation";
 import "./index.css";
 import { getUser } from "./testAuth/AuthApi";
 import { useNavigate } from "react-router-dom";
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
 function SalleAttente() {
   const navigate = useNavigate();
 
   const [code, setCode] = useState("");
   const [host, setHost] = useState("");
+  const [socket, setSocket] = useState(null);
   const [players, setPlayers] = useState([]);
 
-  useEffect(() => {
-    // Créer une instance de socket.io-client et se connecter au serveur
-    const socket = io('http://localhost:5000');
-
-    // Récupérer le code de partie à partir de l'URL
+  async function fetchPlayers() {
     const codePartie = window.location.search.substr(1);
+    const response = await fetch(`http://localhost:5000/game/${codePartie}`);
+    if (!response.ok) {
+      const message = `An error has occurred: ${response.statusText}`;
+      window.alert(message);
+      return;
+    }
+    const game = await response.json();
+    setCode(game.code);
+    setHost(game.host);
+    const nbJ = game.nbJoueurs;
+    //setPlayers(game.joueurs);
+    /*
+    if (game.nbJoueurs >= nbJ) {
+      window.alert("La partie est pleine.");
+      navigate("/");
+    }
+    */
+  }
 
-    // Écouter l'événement "updatePlayers" pour mettre à jour la liste des joueurs
-    socket.on('updatePlayers', (game) => {
-      setCode(game.code);
-      setHost(game.host);
-      setPlayers(game.joueurs);
+  useEffect(() => {
+    fetchPlayers();
+
+    // Connexion à socket.io
+    const socket = io("http://localhost:5000", { transports: ["websocket"] });
+    setSocket(socket);
+
+    socket.on("update-players", (joueurs) => {
+      console.log('Received updated player list:', joueurs);
+      setPlayers(joueurs);
+      console.log(players);
     });
 
-    // Émettre l'événement "joinGame" pour rejoindre la salle d'attente
-    socket.emit('joinGame', codePartie);
+    socket.emit("join-room", { roomId: code, username: getUser() });
 
-    // Nettoyer l'instance de socket lorsque le composant est démonté
     return () => {
+      socket.emit("leave-room", { roomId: code, username: getUser() });
       socket.disconnect();
     };
-  }, []);
+  }, [code]);
 
+  function startGame() {
+    socket.emit("start-game", code);
+  }
+
+  function joueursCo() {
+    return (
+      <div>
+        <p>Joueurs</p>
+      </div>
+    );
+  }
+/*<p> Joueur 1 : {players.players[0]}
+        <br/>Joueur 2 : {players.players[1]}
+        <br/>Joueur 3 : {players.players[2]}
+        <br/>Joueur 4 : {players.players[3]}</p>*/
   return (
     <div className="body">
       <Navigation />
@@ -43,14 +78,13 @@ function SalleAttente() {
         <div className="Douapolis">
           <center>
             <h1> Salle d'attente</h1>
+            <button onClick={startGame}>Commencer la partie</button>
           </center>
         </div>
         <div className="Centre">
           <p>Le code partie est : {code}</p>
           <p>L'hôte est : {host}</p>
-          {players.map((player, index) => (
-            <p key={index}>{player}</p>
-          ))}
+          {joueursCo()}
         </div>
       </Container>
     </div>
